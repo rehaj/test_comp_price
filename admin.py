bdc0348d94add91ade58ee7c0ac36ec9bd93e27c
Redouane <redouane.hajjar@gmail.com>
2026-02-07 14:55:09 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,5 @@
-
+Crée une petite interface d’admin en Streamlit, en Python.
+J’ai un script qui scrape des prix de magazines.
+L’interface doit afficher un tableau avec les noms des magazines et leurs prix.
+Ajouter un bouton ‘Mettre à jour les prix’ qui relance le scraping.
+Ajouter un bouton ‘Sauvegarder’ si je change un prix manuellement.
